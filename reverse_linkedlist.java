afc8dfc6e65fc597ac42b85024f203e2853b6d64
PrathamShamra1111 <sharmapratham223@gmail.com>
2024-01-28 18:20:28 +0530

    reverse linkedlist

---
+++ reverse_linkedlist.java
@@ -0,0 +1,72 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = newNode;
+    }
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public void reverse() {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        head = prev;
+    }
+}
+
+public class reverse_linkedlist {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.append(1);
+        linkedList.append(2);
+        linkedList.append(3);
+        linkedList.append(4);
+
+        System.out.println("Original linked list:");
+        linkedList.display();
+
+        linkedList.reverse();
+
+        System.out.println("Reversed linked list:");
+        linkedList.display();
+    }
+}
