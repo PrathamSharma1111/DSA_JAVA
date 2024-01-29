70a8c9d926bff78964ca2d1372955b2382a7ddc2
PrathamShamra1111 <sharmapratham223@gmail.com>
2024-01-29 20:58:04 +0530

    some changes in insertion

--- LinkedList_Insertion.java
+++ LinkedList_Insertion.java
@@ -1,21 +1,95 @@
-import java.util.LinkedList;
+
 
 public class LinkedList_Insertion {
+    public static class Node {
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static class linkedlist{
+        Node head = null;
+        int size = 0;
+        // inserting a node at head: 
+        Node insertAtHead(int data){
+            Node newNode = new Node(data);
+            if(head == null){
+                head = newNode;
+            }
+            else{
+                newNode.next = head;
+                head = newNode;
+            }
+            size++;
+            return head;
+        }
+
+        // inserting a node at end: 
+        Node insertAtTail(int data){
+            Node newNode = new Node(data);
+            Node temp = head;
+            while(temp.next!=null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+            size++;
+            return head;
+        }
+
+        // insert at index:
+        Node insertAtIndex(int data, int index){
+            Node newnNode = new Node(data);
+            Node temp = head;
+            if(index == 0){
+                insertAtHead(data);
+                return head;
+            }
+            if(index == size){
+                insertAtTail(data);
+                return head;
+            }
+            for(int i=1; i<index; i++){
+                temp = temp.next;
+            }
+            newnNode.next = temp.next;
+            temp.next = newnNode;
+            size++;
+            return head;
+        }
+
+        // display list: 
+        public void display(){
+            Node temp =  head;
+            while(temp!=null){
+                System.out.print(temp.data+" ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        // Creating a LinkedList
-        LinkedList<Number> linkedList = new LinkedList<>();
+        linkedlist list = new linkedlist();
+        list.insertAtHead(10);
+        list.display();
+        list.insertAtTail(100);
+        list.insertAtTail(40);
+        list.insertAtTail(501);
+        list.insertAtTail(99);
+        list.display();
+        // 10 100 40 501 99
+        
+        list.insertAtIndex(80, 4);
+        list.display();
+        // 10 100 40 501 80 99
 
-        // Adding elements to the LinkedList
-        linkedList.add(3);
-        linkedList.add(6);
-        linkedList.add(8);
+        list.insertAtIndex(5, 0);
+        list.display();
+        // 5 10 100 40 501 80 99
 
-        System.out.println("Original LinkedList: " + linkedList);
-        linkedList.addFirst(1);
-        System.out.println("LinkedList after adding at the beginning: " + linkedList);
-        linkedList.add(2, 2);
-        System.out.println("LinkedList after adding at index 2: " + linkedList);
-        linkedList.addLast(9);
-        System.out.println("LinkedList after adding at the end: " + linkedList);
+        list.insertAtIndex(107, 7);
+        list.display();
+        System.out.println(list.size);
     }
 }
